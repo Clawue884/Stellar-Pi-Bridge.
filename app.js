ff29eef64c5c4eff63ddaa069368d32ea93d56b2
Kapten boneng <iwak01bogo@gmail.com>
2024-11-06 21:25:26 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = require('./routes');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
